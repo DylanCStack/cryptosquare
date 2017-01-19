c4d8fbf990086baacaf1bd4e645800f834be436d
David Quisenberry and Dylan Stackhouse <david+dylan@>
2017-01-19 13:35:54 -0800

    add business logic to return back scrambled string on perfect square

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 var scrambleMsg = function (string){
+  var msg = string.replace(/ /g,'');
+  msg = msg.toLowerCase();
+  var encMsg = "";
+  var length = Math.sqrt(msg.length);
+  for (var x = 0; x < length; x++){
+    for (var y = 0; y < length; y++){//length will need to be height
+      encMsg += msg[x+length*y];
+    }
+  }
   return encMsg;
 };
 
